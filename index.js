7126f6ac3478648908f663c36c658f2808211948
Fabricio Robles <fabrobles92@gmail.com>
2022-07-01 16:38:46 -0600

    Version for heroku

--- index.js
+++ index.js
@@ -61,6 +61,18 @@ app.get('/api', (req, res) => {
   res.send({message: 'hola'})
 })
 
+
+if(process.env.NODE_ENV === 'production'){
+  //Express will serve up production assets 
+  //like our main.js or main.css
+  app.use(express.static('client/build'));
+  //Express will serve up the index.html file
+  //if it doesnt recognize the route
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000
 
 http.listen(PORT, () => console.log('Listening'));
